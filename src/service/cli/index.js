c934c696dd7b238555967a1f34d533477b0e6846
titov-andrei <titov_andrei@mail.ru>
2020-01-21 18:11:58 +0300

    Update index.js

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,5 +1,15 @@
 "use strict";
 
-require(`./src/service/cli/version`);
-require(`./src/service/cli/help`);
-require(`./src/service/cli/generate`);
+const help = require(`./help`);
+const generate = require(`./generate`);
+const version = require(`./version`);
+
+const Cli = {
+  [generate.name]: generate,
+  [help.name]: help,
+  [version.name]: version
+};
+
+module.exports = {
+  Cli
+};
