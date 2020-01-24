432673de9fbef037f5a197b38e21b3ca42d57952
titov-andrei <titov_andrei@mail.ru>
2020-01-24 15:30:23 +0300

    Update generate.js

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,78 +1,26 @@
 "use strict";
 
+const { ExitCode } = require(`../constants`);
+const { generateOffers, makeMockData } = require(`../utils`);
+
 const DEFAULT_COUNT = 1;
-const FILE_NAME = `mocks.json`;
-const { getRandomInt, shuffle } = require(`../../utils`);
-const fs = require(`fs`);
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`
-];
-
-const CATEGORIES = [`Книги`, `Разное`, `Посуда`, `Игры`, `Животные`, `Журналы`];
-
-const OfferType = {
-  offer: `offer`,
-  sale: `sale`
-};
-
-const SumRestrict = {
-  min: 1000,
-  max: 100000
-};
+const MAX_COUNT = 1000;
+const FILE_NAME = `mock.json`;
 
 module.exports = {
   name: `--generate`,
-  run(args) {
-    const [count] = args;
+  run(userIndex) {
+    const [count] = userIndex;
+
+    if (count > MAX_COUNT) {
+      console.error(`Не больше ${MAX_COUNT} объявлений`);
+      process.exit(ExitCode.fail);
+    }
+
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer));
+
+    makeMockData(FILE_NAME, content);
+    process.exit(ExitCode.success);
   }
 };
-
-const generateOffers = count =>
-  Array(count)
-    .fill({})
-    .map(() => ({
-      category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-      description: shuffle(SENTENCES)
-        .slice(1, 5)
-        .join(` `),
-      picture: getPictureFileName(
-        getRandomInt(PictureRestrict.min, PictureRestrict.max)
-      ),
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
-      type: Object.keys(OfferType)[
-        Math.floor(Math.random() * Object.keys(OfferType).length)
-      ],
-      sum: getRandomInt(SumRestrict.min, SumRestrict.max)
-    }));
-
-const [count] = args;
-const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-const content = JSON.stringify(generateOffers(countOffer));
-
-fs.writeFile(FILE_NAME, content, err => {
-  if (err) {
-    return console.error(`Can't write data to file...`);
-  }
-
-  return console.info(`Operation success. File created.`);
-});
