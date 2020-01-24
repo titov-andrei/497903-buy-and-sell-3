519c24abf4e78b7c584ac7d8e389c4e3baa6751f
titov-andrei <titov_andrei@mail.ru>
2020-01-24 15:30:19 +0300

    Update service.js

--- src/service/service.js
+++ src/service/service.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const { Cli } = require(`./cli`);
-const { DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode } = require(`../constants`);
+const { DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode } = require(`./constants`);
 
 const userArguments = process.argv.slice(USER_ARGV_INDEX);
-const [userCommand] = userArguments;
+const [command] = userArguments;
 
-if (userArguments.length === 0 || !Cli[userCommand]) {
+if (userArguments.length === 0 || !Cli[command]) {
   Cli[DEFAULT_COMMAND].run();
   process.exit(ExitCode.success);
 }
 
-Cli[userCommand].run(userArguments.slice(1));
+Cli[command].run(userArguments.slice(1));
