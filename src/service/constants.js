a261df4930bfcf92e1840ae7819a07416bd12c44
titov-andrei <titov_andrei@mail.ru>
2020-07-09 14:42:35 +0300

    Update constants.js

--- src/service/constants.js
+++ src/service/constants.js
@@ -6,9 +6,17 @@ const ExitCode = {
   success: 0,
   fail: 1
 };
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  HttpCode
 };
