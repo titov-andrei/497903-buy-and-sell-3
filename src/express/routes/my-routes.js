12ef2094c3d39fd1778b566889ad58ac1173c727
titov-andrei <titov_andrei@mail.ru>
2020-08-01 16:12:31 +0300

    ...

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const { Router } = require(`express`);
 const myRoutes = new Router();
 
-myRoutes.get(`/`, (req, res) => res.send(`/my`));
-myRoutes.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRoutes.get(`/`, (req, res) => res.render(`my-tickets`));
+myRoutes.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRoutes;
