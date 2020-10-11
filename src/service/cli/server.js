a484afbf2eedba716ad8ec7c36e08599360f4775
titov-andrei <titov_andrei@mail.ru>
2020-10-11 12:36:23 +0300

    Update server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -22,7 +22,7 @@ app.get(`/offers`, async (req, res) => {
     const mocks = JSON.parse(fileContent);
     res.json(mocks);
   } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+    res.send([]);
   }
 });
 
