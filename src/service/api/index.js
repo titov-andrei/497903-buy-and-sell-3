eaf2d0c6faed91fbe628e6fd9db30eb553a5e11a
titov-andrei <titov_andrei@mail.ru>
2020-11-03 15:25:20 +0300

    Create index.js

---
+++ src/service/api/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { Router } = require(`express`);
+const category = require(`../api/category`);
+const offer = require(`../api/offer`);
+const search = require(`../api/search`);
+
+const getMockData = require(`../lib/get-mock-data`);
+
+const {
+  CategoryService,
+  OfferService,
+  CommentService,
+  SearchService,
+} = require(`../data-service`);
+
+const app = new Router();
+
+(async () => {
+  const mockData = await getMockData();
+
+  category(app, new CategoryService(mockData));
+  offer(app, new OfferService(mockData), new CommentService());
+  search(app, new SearchService(mockData));
+})();
+
+module.exports = { app };
