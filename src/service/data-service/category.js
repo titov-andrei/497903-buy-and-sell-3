2dfaa6b6675fb9f10b937b37b428e64ced80722e
titov-andrei <titov_andrei@mail.ru>
2020-11-03 15:36:54 +0300

    Create category.js

---
+++ src/service/data-service/category.js
@@ -0,0 +1,16 @@
+class CategoryService {
+  constructor(offers) {
+    this._offers = offers;
+  }
+
+  findAll() {
+    const categories = this._offers.reduce((acc, offer) => {
+      acc.add(...offer.category);
+      return acc;
+    }, new Set());
+
+    return [...categories];
+  }
+}
+
+module.exports = { CategoryService };
