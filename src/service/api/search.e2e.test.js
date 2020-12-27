1c690962031fb93aa5d62935cd5cf7763b850e25
titov-andrei <titov_andrei@mail.ru>
2020-12-27 11:28:58 +0300

    Update search.e2e.test.js

--- src/service/api/search.e2e.test.js
+++ src/service/api/search.e2e.test.js
@@ -173,3 +173,15 @@ describe(`API returns offer based on search query`, () => {
   test(`Offer has correct id`, () =>
     expect(response.body[0].id).toBe(`dTYKZm`));
 });
+
+test(`API returns code 404 if nothing is found`, () =>
+  request(app)
+    .get(`/search`)
+    .query({
+      query: `Продам свою душу`,
+    })
+    .expect(HttpCode.NOT_FOUND));
+
+test(`API returns 400 when query string is absent`, () =>
+  
+  request(app).get(`/search`).expect(HttpCode.BAD_REQUEST));
