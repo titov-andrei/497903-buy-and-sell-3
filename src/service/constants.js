5e8af216c8533c0631f267cf2a7a3ad5c8e1c12a
titov-andrei <titov_andrei@mail.ru>
2020-12-27 13:47:18 +0300

    Update constants.js

--- src/service/constants.js
+++ src/service/constants.js
@@ -8,10 +8,12 @@ const ExitCode = {
 };
 const HttpCode = {
   OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
-  FORBIDDEN: 403,
-  UNAUTHORIZED: 401,
 };
 
 module.exports = {
