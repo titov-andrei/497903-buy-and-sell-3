347791cf4a9863cc49e03aed37ec0aafa7cb5f75
titov-andrei <titov_andrei@mail.ru>
2021-01-07 15:17:19 +0300

    Update logger.js

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -3,7 +3,7 @@
 const pino = require(`pino`);
 const { Env } = require(`../constants`);
 
-const LOG_FILE = `../logs/api.log`;
+const LOG_FILE = `./src/service/logs/api.log`;
 const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
 const defaultLogLevel = isDevMode ? `info` : `error`;
 
