9a48dd3b569f4294c38cbc0c54a7dcbf0ede90e0
titov-andrei <titov_andrei@mail.ru>
2021-02-07 14:16:58 +0300

    Update main-routes.js

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,8 +2,12 @@
 
 const { Router } = require(`express`);
 const mainRoutes = new Router();
+const api = require(`../api`).getAPI();
 
-mainRoutes.get(`/`, (req, res) => res.render(`main`));
+mainRoutes.get(`/`, async (req, res) => {
+  const pugOffers = await api.getOffers();
+  res.render(`main`, { pugOffers });
+});
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search-result`));
