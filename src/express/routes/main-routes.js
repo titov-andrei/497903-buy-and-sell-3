1f95bfed4bb0ce12b9b1631b8a5bdc7535e2ffba
titov-andrei <titov_andrei@mail.ru>
2021-02-13 11:49:20 +0300

    Update main-routes.js

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,15 +1,31 @@
 "use strict";
 
+// Подключаем и инициализируем экземпляр Router
 const { Router } = require(`express`);
 const mainRoutes = new Router();
 const api = require(`../api`).getAPI();
 
+// Определяем `GET` маршруты.
+// В качестве ответа отправляем путь маршрута.
 mainRoutes.get(`/`, async (req, res) => {
   const pugOffers = await api.getOffers();
   res.render(`main`, { pugOffers });
 });
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
-mainRoutes.get(`/search`, (req, res) => res.render(`search-result`));
+mainRoutes.get(`/search`, async (req, res) => {
+  try {
+    const { search } = req.query;
+    const results = await api.search(search);
+
+    res.render(`search-result`, {
+      results,
+    });
+  } catch (error) {
+    res.render(`search-result`, {
+      results: [],
+    });
+  }
+});
 
 module.exports = mainRoutes;
