9f0333d049e8e15fa1187d89b62aad07b2f92d64
titov-andrei <titov_andrei@mail.ru>
2021-04-29 18:44:53 +0300

    Create index.js

---
+++ src/service/models/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { Model } = require(`sequelize`);
+const defineCategory = require(`./category`);
+const defineComment = require(`./comment`);
+const defineOffer = require(`./offer`);
+
+// импортируем перечисление
+const Aliase = require(`./aliase`);
+
+class OfferCategory extends Model {}
+
+const define = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Offer = defineOffer(sequelize);
+
+  Offer.hasMany(Comment, { as: Aliase.COMMENTS, foreignKey: `offerId` });
+  Comment.belongsTo(Offer, { foreignKey: `offerId` });
+
+  OfferCategory.init({}, { sequelize });
+
+  Offer.belongsToMany(Category, {
+    through: `offerCategories`,
+    as: Aliase.CATEGORIES,
+  });
+  Category.belongsToMany(Offer, {
+    through: `offerCategories`,
+    as: Aliase.OFFERS,
+  });
+  Category.hasMany(OfferCategory, { as: Aliase.OFFER_CATEGORIES });
+
+  return { Category, Comment, Offer, OfferCategory };
+};
+
+module.exports = define;
