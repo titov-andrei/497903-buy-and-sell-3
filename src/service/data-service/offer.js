8037cc904248bc0585aaf244ff60c10590e46c64
titov-andrei <titov_andrei@mail.ru>
2021-04-29 18:44:26 +0300

    Update offer.js

--- src/service/data-service/offer.js
+++ src/service/data-service/offer.js
@@ -1,46 +1,45 @@
 "use strict";
 
-const { nanoid } = require(`nanoid`);
-const { MAX_ID_LENGTH } = require(`../constants`);
+const Aliase = require(`../models/aliase`);
 
 class OfferService {
-  constructor(offers) {
-    this._offers = offers;
+  constructor(sequelize) {
+    this._Offer = sequelize.models.Offer;
+    this._Comment = sequelize.models.Comment;
+    this._Category = sequelize.models.Category;
   }
 
-  findAll() {
-    return this._offers;
+  async create(offerData) {
+    const offer = await this._Offer.create(offerData);
+    await offer.addCategories(offerData.categories);
+    return offer.get();
+  }
+
+  async drop(id) {
+    const deletedRows = await this._Offer.destroy({
+      where: { id },
+    });
+    return !!deletedRows;
   }
 
   findOne(id) {
-    return this._offers.find((item) => item.id === id);
+    return this._Offer.findByPk(id, { include: [Aliase.CATEGORIES] });
   }
 
-  create(offer) {
-    const newOffer = Object.assign(
-      { id: nanoid(MAX_ID_LENGTH), comments: [] },
-      offer
-    );
-
-    this._offers.push(newOffer);
-    return newOffer;
+  async update(id, offer) {
+    const [affectedRows] = await this._Offer.update(offer, {
+      where: { id },
+    });
+    return !!affectedRows;
   }
 
-  update(id, offer) {
-    const oldOffer = this._offers.find((item) => item.id === id);
-
-    return Object.assign(oldOffer, offer);
-  }
-
-  drop(id) {
-    const offer = this._offers.find((item) => item.id === id);
-
-    if (!offer) {
-      return null;
+  async findAll(needComments) {
+    const include = [Aliase.CATEGORIES];
+    if (needComments) {
+      include.push(Aliase.COMMENTS);
     }
-
-    this._offers = this._offers.filter((item) => item.id !== id);
-    return offer;
+    const offers = await this._Offer.findAll({ include });
+    return offers.map((item) => item.get());
   }
 }
 
