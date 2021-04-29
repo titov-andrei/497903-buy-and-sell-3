44b8e7d66a301f2d756f2ea48b2f925d6148f0b3
titov-andrei <titov_andrei@mail.ru>
2021-04-29 18:43:45 +0300

    Update category.js

--- src/service/api/category.js
+++ src/service/api/category.js
@@ -8,8 +8,9 @@ const route = new Router();
 module.exports = (app, service) => {
   app.use(`/category`, route);
 
-  route.get(`/`, (req, res) => {
-    const categories = service.findAll();
+  route.get(`/`, async (req, res) => {
+    const { count } = req.query;
+    const categories = await service.findAll(count);
     res.status(HttpCode.OK).json(categories);
   });
 };
