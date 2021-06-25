3a2128c761c8634c9a0a89f58539314ff52e0bb6
titov-andrei <titov_andrei@mail.ru>
2021-06-25 15:07:12 +0300

    Update offers-routes.js

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -29,13 +29,16 @@ const storage = multer.diskStorage({
 // без `offers`, т.к. уже указали при подключении модуля маршрута
 // в `index.js`.
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+
 offersRouter.get(`/add`, async (req, res) => {
+  const { error } = req.query;
   const categories = await api.getCategories();
-  res.render(`new-ticket`, { categories });
+  res.render(`new-ticket`, { categories, error });
 });
 const upload = multer({ storage });
 
 offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const { user } = req.session;
   const { body, file } = req;
   const offerData = {
     picture: file.filename,
@@ -43,29 +46,70 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
     type: body.action,
     description: body.comment,
     title: body[`ticket-name`],
-    categories: body.category,
+    categories: ensureArray(body.category),
+    userId: user.id,
   };
   try {
     await api.createOffer(offerData);
     res.redirect(`/my`);
   } catch (error) {
-    res.redirect(`back`);
+    res.redirect(
+      `/offers/add?error=${encodeURIComponent(error.response.data)}`
+    );
   }
 });
 
 offersRouter.get(`/edit/:id`, async (req, res) => {
   const { id } = req.params;
+  const { error } = req.query;
   const [offer, categories] = await Promise.all([
     api.getOffer(id),
     api.getCategories(),
   ]);
-  res.render(`ticket-edit`, { offer, categories });
+  res.render(`ticket-edit`, { id, offer, categories, error });
+});
+
+offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
+  const { body, file } = req;
+  const { id } = req.params;
+  const offerData = {
+    picture: file ? file.filename : body[`old-image`],
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    categories: ensureArray(body.category),
+  };
+
+  try {
+    await api.editOffer(id, offerData);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(
+      `/offers/edit/${id}?error=${encodeURIComponent(error.response.data)}`
+    );
+  }
 });
 
 offersRouter.get(`/:id`, async (req, res) => {
   const { id } = req.params;
+  const { error } = req.query;
   const offer = await api.getOffer(id, true);
-  res.render(`offers/ticket`, { offer });
+  res.render(`offers/ticket`, { offer, id, error });
+});
+
+offersRouter.post(`/:id/comments`, async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+
+  try {
+    await api.createComment(id, { text: comment });
+    res.redirect(`/offers/${id}`);
+  } catch (error) {
+    res.redirect(
+      `/offers/${id}?error=${encodeURIComponent(error.response.data)}`
+    );
+  }
 });
 
 module.exports = offersRouter;
