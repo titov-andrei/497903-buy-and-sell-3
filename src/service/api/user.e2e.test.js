2a23a0e49fe61400af4e81544881c58913df7fa5
titov-andrei <titov_andrei@mail.ru>
2021-06-25 17:39:00 +0300

    Create user.e2e.test.js

---
+++ src/service/api/user.e2e.test.js
@@ -0,0 +1,240 @@
+"use strict";
+
+const express = require(`express`);
+const request = require(`supertest`);
+const Sequelize = require(`sequelize`);
+
+const initDB = require(`../lib/init-db`);
+const user = require(`./user`);
+const DataService = require(`../data-service/user`);
+
+const { HttpCode } = require(`../constants`);
+
+const mockCategories = [`Животные`, `Журналы`, `Игры`];
+
+const mockUsers = [
+  {
+    name: `Иван Иванов`,
+    email: `ivanov@example.com`,
+    passwordHash: await passwordUtils.hash(`ivanov`),
+    avatar: `avatar01.jpg`,
+  },
+  {
+    name: `Пётр Петров`,
+    email: `petrov@example.com`,
+    passwordHash: await passwordUtils.hash(`petrov`),
+    avatar: `avatar02.jpg`,
+  },
+];
+
+const mockOffers = [
+  {
+    user: `ivanov@example.com`,
+    categories: [`Игры`, `Журналы`],
+    comments: [
+      {
+        user: `petrov@example.com`,
+        text: `С чем связана продажа? Почему так дешёво? Неплохо, но дорого. А где блок питания?`,
+      },
+      {
+        user: `ivanov@example.com`,
+        text: `А где блок питания?`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `Оплата наличными или перевод на карту? Неплохо, но дорого. Почему в таком ужасном состоянии?`,
+      },
+    ],
+    description: `Бонусом отдам все аксессуары. Если товар не понравится — верну всё до последней копейки. Товар в отличном состоянии. Это настоящая находка для коллекционера!`,
+    picture: `item13.jpg`,
+    title: `Куплю антиквариат`,
+    type: `OFFER`,
+    sum: 10030,
+  },
+  {
+    user: `petrov@example.com`,
+    categories: [`Игры`],
+    comments: [
+      {
+        user: `ivanov@example.com`,
+        text: `А где блок питания? С чем связана продажа? Почему так дешёво?`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `А сколько игр в комплекте?`,
+      },
+      {
+        user: `ivanov@example.com`,
+        text: `Оплата наличными или перевод на карту? Вы что?! В магазине дешевле.`,
+      },
+    ],
+    description: `Это настоящая находка для коллекционера! Если найдёте дешевле — сброшу цену. Продаю с болью в сердце... Товар в отличном состоянии.`,
+    picture: `item06.jpg`,
+    title: `Куплю породистого кота`,
+    type: `OFFER`,
+    sum: 6694,
+  },
+  {
+    user: `ivanov@example.com`,
+    categories: [`Журналы`, `Животные`],
+    comments: [
+      {
+        user: `petrov@example.com`,
+        text: `Совсем немного... А сколько игр в комплекте? Неплохо, но дорого.`,
+      },
+    ],
+    description: `Пользовались бережно и только по большим праздникам. Продаю с болью в сердце... Даю недельную гарантию. Если товар не понравится — верну всё до последней копейки.`,
+    picture: `item16.jpg`,
+    title: `Продам слона`,
+    type: `SALE`,
+    sum: 87784,
+  },
+  {
+    user: `petrov@example.com`,
+    categories: [`Игры`],
+    comments: [
+      {
+        user: `ivanov@example.com`,
+        text: `А сколько игр в комплекте? Продаю в связи с переездом. Отрываю от сердца.`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `С чем связана продажа? Почему так дешёво?`,
+      },
+    ],
+    description: `При покупке с меня бесплатная доставка в черте города. Бонусом отдам все аксессуары. Если найдёте дешевле — сброшу цену. Продаю с болью в сердце...`,
+    picture: `item13.jpg`,
+    title: `Продам отличную подборку фильмов на VHS`,
+    type: `SALE`,
+    sum: 54264,
+  },
+  {
+    user: `ivanov@example.com`,
+    categories: [`Животные`],
+    comments: [
+      {
+        user: `ivanov@example.com`,
+        text: `Оплата наличными или перевод на карту?`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `Почему в таком ужасном состоянии? Совсем немного...`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `Продаю в связи с переездом. Отрываю от сердца. Оплата наличными или перевод на карту? Вы что?! В магазине дешевле.`,
+      },
+      {
+        user: `petrov@example.com`,
+        text: `С чем связана продажа? Почему так дешёво? А сколько игр в комплекте?`,
+      },
+    ],
+    description: `Таких предложений больше нет! Даю недельную гарантию. Продаю с болью в сердце... Пользовались бережно и только по большим праздникам.`,
+    picture: `item15.jpg`,
+    title: `Куплю породистого кота`,
+    type: `OFFER`,
+    sum: 91863,
+  },
+];
+
+const createAPI = async () => {
+  const mockDB = new Sequelize(`sqlite::memory:`, { logging: false });
+  await initDB(mockDB, {
+    categories: mockCategories,
+    offers: mockOffers,
+    users: mockUsers,
+  });
+  const app = express();
+  app.use(express.json());
+  user(app, new DataService(mockDB));
+  return app;
+};
+
+describe(`API creates user if data is valid`, () => {
+  const validUserData = {
+    name: `Сидор Сидоров`,
+    email: `sidorov@example.com`,
+    password: `sidorov`,
+    passwordRepeated: `sidorov`,
+    avatar: `sidorov.jpg`,
+  };
+
+  let response;
+
+  beforeAll(async () => {
+    let app = await createAPI();
+    response = await request(app).post(`/user`).send(validUserData);
+  });
+
+  test(`Status code 201`, () =>
+    expect(response.statusCode).toBe(HttpCode.CREATED));
+});
+
+describe(`API refuses to create user if data is invalid`, () => {
+  const validUserData = {
+    name: `Сидор Сидоров`,
+    email: `sidorov@example.com`,
+    password: `sidorov`,
+    passwordRepeated: `sidorov`,
+    avatar: `sidorov.jpg`,
+  };
+
+  let app;
+
+  beforeAll(async () => {
+    app = await createAPI();
+  });
+
+  test(`Without any required property response code is 400`, async () => {
+    for (const key of Object.keys(validUserData)) {
+      const badUserData = { ...validUserData };
+      delete badUserData[key];
+      await request(app)
+        .post(`/user`)
+        .send(badUserData)
+        .expect(HttpCode.BAD_REQUEST);
+    }
+  });
+
+  test(`When field type is wrong response code is 400`, async () => {
+    const badUsers = [
+      { ...validUserData, firstName: true },
+      { ...validUserData, email: 1 },
+    ];
+    for (const badUserData of badUsers) {
+      await request(app)
+        .post(`/user`)
+        .send(badUserData)
+        .expect(HttpCode.BAD_REQUEST);
+    }
+  });
+
+  test(`When field value is wrong response code is 400`, async () => {
+    const badUsers = [
+      { ...validUserData, password: `short`, passwordRepeated: `short` },
+      { ...validUserData, email: `invalid` },
+    ];
+    for (const badUserData of badUsers) {
+      await request(app)
+        .post(`/user`)
+        .send(badUserData)
+        .expect(HttpCode.BAD_REQUEST);
+    }
+  });
+
+  test(`When password and passwordRepeated are not equal, code is 400`, async () => {
+    const badUserData = { ...validUserData, passwordRepeated: `not sidorov` };
+    await request(app)
+      .post(`/user`)
+      .send(badUserData)
+      .expect(HttpCode.BAD_REQUEST);
+  });
+
+  test(`When email is already in use status code is 400`, async () => {
+    const badUserData = { ...validUserData, email: `ivanov@example.com` };
+    await request(app)
+      .post(`/user`)
+      .send(badUserData)
+      .expect(HttpCode.BAD_REQUEST);
+  });
+});
