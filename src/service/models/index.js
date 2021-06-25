fd85a5ad2bedde6e1c9194801df26af27c235406
titov-andrei <titov_andrei@mail.ru>
2021-06-25 17:40:18 +0300

    Update index.js

--- src/service/models/index.js
+++ src/service/models/index.js
@@ -4,6 +4,7 @@ const { Model } = require(`sequelize`);
 const defineCategory = require(`./category`);
 const defineComment = require(`./comment`);
 const defineOffer = require(`./offer`);
+const defineUser = require(`./user`);
 
 // импортируем перечисление
 const Aliase = require(`./aliase`);
@@ -14,9 +15,16 @@ const define = (sequelize) => {
   const Category = defineCategory(sequelize);
   const Comment = defineComment(sequelize);
   const Offer = defineOffer(sequelize);
+  const User = defineUser(sequelize);
 
   Offer.hasMany(Comment, { as: Aliase.COMMENTS, foreignKey: `offerId` });
   Comment.belongsTo(Offer, { foreignKey: `offerId` });
+
+  User.hasMany(Offer, {as: Aliase.OFFERS, foreignKey: `userId`});
+  Offer.belongsTo(User, {as: Aliase.USER, foreignKey: `userId`});
+
+  User.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `userId`});
+  Comment.belongsTo(User, {as: Aliase.USER, foreignKey: `userId`});
 
   OfferCategory.init({}, { sequelize });
 
