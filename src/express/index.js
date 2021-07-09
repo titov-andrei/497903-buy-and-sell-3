9b723a251dbcd544d0be52770d9610683192baa9
titov-andrei <titov_andrei@mail.ru>
2021-07-09 15:07:44 +0300

    Update index.js

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require(`express`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
 
 // Маршруты приложения мы опишем в отдельных файлах.
 // Для определения маршрутов мы воспользуемся Router().
@@ -14,6 +15,8 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 
+const sequelize = require(`../service/lib/sequelize`);
+
 // Подключаем встроенный модуль path
 const path = require(`path`);
 
@@ -22,6 +25,31 @@ const PUBLIC_DIR = `public`;
 
 // Зафиксируем папку для загрузки файлов
 const UPLOAD_DIR = `upload`;
+
+const { SESSION_SECRET } = process.env;
+if (!SESSION_SECRET) {
+  throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
+const mySessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 180000,
+  checkExpirationInterval: 60000,
+});
+
+sequelize.sync({ force: false });
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    store: mySessionStore,
+    resave: false,
+    proxy: true,
+    saveUninitialized: false,
+  })
+);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
