e314d9f88a7af5dabcd0fba9dbf0904e21c94421
titov-andrei <titov_andrei@mail.ru>
2021-07-09 15:08:33 +0300

    Update user.js

--- src/service/api/user.js
+++ src/service/api/user.js
@@ -21,4 +21,23 @@ module.exports = (app, service) => {
 
     res.status(HttpCode.CREATED).json(result);
   });
+
+  route.post(`/auth`, async (req, res) => {
+    const { email, password } = req.body;
+    const user = await service.findByEmail(email);
+    if (!user) {
+      res.status(HttpCode.UNAUTHORIZED).send(`Email is incorrect`);
+      return;
+    }
+    const passwordIsCorrect = await passwordUtils.compare(
+      password,
+      user.passwordHash
+    );
+    if (passwordIsCorrect) {
+      delete user.passwordHash;
+      res.status(HttpCode.OK).json(user);
+    } else {
+      res.status(HttpCode.UNAUTHORIZED).send(`Password is incorrect`);
+    }
+  });
 };
