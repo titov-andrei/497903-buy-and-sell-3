d2bf9f575b1b51b76138bc0045b5eb0cdbceb4db
titov-andrei <titov_andrei@mail.ru>
2021-07-09 15:08:00 +0300

    Update offers-routes.js

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -4,10 +4,14 @@
 const { Router } = require(`express`);
 const offersRouter = new Router();
 const api = require(`../api`).getAPI();
+const auth = require(`../middlewares/auth`);
+const { ensureArray } = require(`../../service/utils`);
 
 const multer = require(`multer`);
 const path = require(`path`);
 const { nanoid } = require(`nanoid`);
+const csrf = require(`csurf`);
+const csrfProtection = csrf();
 
 const UPLOAD_DIR = `../upload/img/`;
 
@@ -28,16 +32,20 @@ const storage = multer.diskStorage({
 // Следует помнить, что в первом параметре мы указываем путь маршрута
 // без `offers`, т.к. уже указали при подключении модуля маршрута
 // в `index.js`.
-offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+offersRouter.get(`/category/:id`, (req, res) => {
+  const { user } = req.session;
+  res.render(`category`, { user });
+});
 
-offersRouter.get(`/add`, async (req, res) => {
+offersRouter.get(`/add`, auth, csrfProtection, async (req, res) => {
+  const { user } = req.session;
   const { error } = req.query;
   const categories = await api.getCategories();
-  res.render(`new-ticket`, { categories, error });
+  res.render(`new-ticket`, { categories, error, user, csrfToken: req.csrfToken() });
 });
 const upload = multer({ storage });
 
-offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+offersRouter.post(`/add`, auth, csrfProtection, upload.single(`avatar`), async (req, res) => {
   const { user } = req.session;
   const { body, file } = req;
   const offerData = {
@@ -59,18 +67,20 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
   }
 });
 
-offersRouter.get(`/edit/:id`, async (req, res) => {
+offersRouter.get(`/edit/:id`, auth, csrfProtection, async (req, res) => {
   const { id } = req.params;
+  const { user } = req.session;
   const { error } = req.query;
   const [offer, categories] = await Promise.all([
     api.getOffer(id),
     api.getCategories(),
   ]);
-  res.render(`ticket-edit`, { id, offer, categories, error });
+  res.render(`ticket-edit`, { id, offer, categories, user, error, csrfToken: req.csrfToken() });
 });
 
-offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
+offersRouter.post(`/edit/:id`, auth, csrfProtection, upload.single(`avatar`), async (req, res) => {
   const { body, file } = req;
+  const { user } = req.session;
   const { id } = req.params;
   const offerData = {
     picture: file ? file.filename : body[`old-image`],
@@ -79,6 +89,7 @@ offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
     description: body.comment,
     title: body[`ticket-name`],
     categories: ensureArray(body.category),
+    userId: user.id
   };
 
   try {
@@ -93,17 +104,19 @@ offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
 
 offersRouter.get(`/:id`, async (req, res) => {
   const { id } = req.params;
+  const { user } = req.session;
   const { error } = req.query;
   const offer = await api.getOffer(id, true);
-  res.render(`offers/ticket`, { offer, id, error });
+  res.render(`offers/ticket`, { offer, id, user, error, csrfToken: req.csrfToken() });
 });
 
-offersRouter.post(`/:id/comments`, async (req, res) => {
+offersRouter.post(`/:id/comments`, auth, csrfProtection, async (req, res) => {
   const { id } = req.params;
+  const { user } = req.session;
   const { comment } = req.body;
 
   try {
-    await api.createComment(id, { text: comment });
+    await api.createComment(id, { userId: user.id, text: comment });
     res.redirect(`/offers/${id}`);
   } catch (error) {
     res.redirect(
