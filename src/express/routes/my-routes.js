e9220726b23b65d07ef4c2e22717f2e67884d17a
titov-andrei <titov_andrei@mail.ru>
2021-07-09 15:07:57 +0300

    Update my-routes.js

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,18 +3,22 @@
 // Подключаем и инициализируем экземпляр Router
 const { Router } = require(`express`);
 const api = require(`../api`).getAPI();
+const auth = require(`../middleware/auth`);
 const myRoutes = new Router();
+myRoutes.use(auth);
 
 // Определяем `GET` маршруты.
 // В качестве ответа отправляем путь маршрута.
 myRoutes.get(`/`, async (req, res) => {
+  const { user } = req.session;
   const pugOffers = await api.getOffers();
-  res.render(`my-tickets`, { pugOffers });
+  res.render(`my-tickets`, { pugOffers, user });
 });
 
 myRoutes.get(`/comments`, async (req, res) => {
+  const { user } = req.session;
   const pugOffers = await api.getOffers({ comments: true });
-  res.render(`comments`, { pugOffers: pugOffers.slice(0, 3) });
+  res.render(`comments`, { pugOffers: pugOffers.slice(0, 3), user });
 });
 
 module.exports = myRoutes;
